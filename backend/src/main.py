66d5f2892fcee46f6f5953685781a0da2e0caffd
RodrigoMaddonni <rfm00@iesemilidarder.com>
2025-12-14 12:16:12 +0100

    Carpeta /backend - FastAPI - Commit 1

---
+++ backend/src/main.py
@@ -0,0 +1,16 @@
+# Importa la clase principal para crear la API
+from fastapi import FastAPI
+
+# Crea la instancia de tu API
+app = FastAPI()
+
+# Define un endpoint que responde a solicitudes GET en /health
+@app.get("/health", status_code=200)
+def health_check():  # Función que se ejecuta cuando alguien visita /health
+    # Devuelve un JSON indicando que todo está bien
+    return {"status": "ok"}
+
+
+
+# para ejecutar y probar api poner el siguiente comando dentro del directorio:
+# uvicorn main:app --reload
